55041fe009c03c6feccf3769400b33fe4edccc4e
taoqi610 <taoqi610@cs.ucla.edu>
2017-12-30 15:43:42 -0800

    fix small bugs

--- models/rnn_product/rnn_product_keras.py
+++ models/rnn_product/rnn_product_keras.py
@@ -1,16 +1,8 @@
 
-from math import sqrt
-from numpy import concatenate
-from matplotlib import pyplot
-from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error
 
 import tensorflow as tf
-
 
 import numpy as np
 import os
@@ -45,10 +37,14 @@ data_cols = [
 ]
 data_dir = './data'
 data = [np.load(os.path.join(data_dir, '{}.npy'.format(i)), mmap_mode='r') for i in data_cols]
-product_data = DataFrame(columns=data_cols, data=data)
+product_data = DataFrame(data=data).transpose()
+product_data.columns = data_cols
 
-train_dataset = product_data.loc[product_data['label'] == 'train']
-test_dataset = product_data.loc[product_data['label'] == 'test']
+print product_data.dtypes
+print 'read!!!!'
+
+# train_dataset = product_data.loc[product_data['label'] == 'train']
+# test_dataset = product_data.loc[product_data['label'] == 'test']
 
 
 num_rows = product_data.shape[0]
@@ -57,14 +53,14 @@ num_rows = product_data.shape[0]
 # product_name_length = np.zeros(shape=[num_rows], dtype=np.int8)
 # history_length = np.zeros(shape=[num_rows], dtype=np.int8)
 
-is_ordered_history = to_categorical(product_data['is_ordered_history'].value)
-index_in_order_history = to_categorical(product_data['index_in_order_history'].value)
-order_dow_history = to_categorical(product_data['order_dow_history'].value)
-order_hour_history = to_categorical(product_data['order_hour_history'].value)
-days_since_prior_order_history = to_categorical(product_data['days_since_prior_order_history'].value)
-order_size_history = to_categorical(product_data['order_size_history'].value)
-reorder_size_history = to_categorical(product_data['reorder_size_history'].value)
-order_number_history = to_categorical(product_data['order_number_history'].value)
+is_ordered_history = to_categorical(product_data['is_ordered_history'], 2)
+index_in_order_history = to_categorical(product_data['index_in_order_history'], 20)
+order_dow_history = to_categorical(product_data['order_dow_history'], 8)
+order_hour_history = to_categorical(product_data['order_hour_history'], 25)
+days_since_prior_order_history = to_categorical(product_data['days_since_prior_order_history'], 31)
+order_size_history = to_categorical(product_data['order_size_history'], 60)
+reorder_size_history = to_categorical(product_data['reorder_size_history'], 50)
+order_number_history = to_categorical(product_data['order_number_history'], 101)
 
 
 x_history = tf.concat([
@@ -76,6 +72,7 @@ x_history = tf.concat([
     order_size_history,
     reorder_size_history,
     order_number_history
+
     # ,
     # index_in_order_history_scalar,
     # order_dow_history_scalar,
@@ -91,18 +88,16 @@ x_history = tf.concat([
 
 
 print 'aaaaaaaaaaaaaaaaaaa'
-# trainX =np.zeros((test_df.shape[0],100, None))
 
-print 'bbbbbbbbbbbbbbbbbb'
 
 
 timesteps = 100
 model = Sequential()
-# # # time step should be 100 for sequence
 
 model.add(Masking(mask_value=0., input_shape=(timesteps, x_history.shape[1] )))
-model.add(LSTM(512))
+model.add(LSTM(128))
 model.add(TimeDistributed(Dense(1)))
 model.compile(loss='binary_crossentropy', optimizer='adam')
+print(model.summary())
 
 
